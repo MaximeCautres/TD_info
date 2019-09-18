e752fa86b0c6f215b71a580307c2d7bca660043f
Maxime Cautrès <maxime.cautres@gmail.com>
2019-09-18 21:51:36 +0200

    TD info spé du 18/09/19

--- Info Spé/Caml/TD2.ml
+++ Info Spé/Caml/TD2.ml
@@ -75,6 +75,80 @@ let rec find_r_l arbre = match arbre with
 
 
 
+(* exercice 3*)
+
+type ope = 
+			| E of int 
+			| N of ope
+			| A of ope*ope
+			| M of ope*ope
+			| D of ope*ope
+			| S of ope*ope;;
 
 
+let rec eval expre = match expre with
+	|E(n) -> n
+	|N(op) -> - (eval op)
+	|A(op1, op2) -> (eval op1) + (eval op2)
+	|M(op1, op2) -> (eval op1) * (eval op2)
+	|D(op1, op2) -> (eval op1) / (eval op2)
+	|S(op1, op2) -> (eval op1) - (eval op2);;
+
+let rec affiche expre = match expre with
+	|E(n) -> string_of_int n
+	|N(op) -> "(-"^(affiche op)^")"
+	|A(op1, op2) -> "("^(affiche op1)^"+"^(affiche op2)^")"
+	|M(op1, op2) -> "("^(affiche op1)^"*"^(affiche op2)^")"
+	|D(op1, op2) -> "("^(affiche op1)^"/"^(affiche op2)^")"
+	|S(op1, op2) -> "("^(affiche op1)^"-"^(affiche op2)^")";;
+	
+affiche (A(M(E(3),E(2)),D(E(4),E(2))));;
+
+let rec affiche_p expre = match expre with
+	|E(n) -> string_of_int n
+	|N(op) -> "_"^(affiche_p op)
+	|A(op1, op2) -> "+"^" "^(affiche_p op1)^" "^(affiche_p op2)
+	|M(op1, op2) -> "*"^" "^(affiche_p op1)^" "^(affiche_p op2)
+	|D(op1, op2) -> "/"^" "^(affiche_p op1)^" "^(affiche_p op2)
+	|S(op1, op2) -> "-"^" "^(affiche_p op1)^" "^(affiche_p op2);;
+
+affiche_p (A(M(E(3),E(2)),D(E(4),E(2))));;
+
+(* exercice 4*)
+
+let rank arbre = 
+	let rec aux1 arbre = match arbre with
+	|Vide -> 0
+	|N(g,_,d)-> 1 + (aux1 g) + (aux1 d) 
+	in let rec aux arbre = match arbre with
+	|Vide -> Vide
+	|N(g,n,d)->N(aux g,(n,1+(aux1 g)), aux d)
+	in aux arbre;;
+
+rank (N(N(N(Vide,1,Vide),3,N(Vide, 5, Vide)),6,N(Vide,7,N(Vide,9,Vide))));;
+
+let rec trouve a arbre = match arbre with
+	|Vide -> false
+	|N(g,n,d)-> if a = n then true else
+					(if a < n then trouve a g 
+					else trouve a d);;
+
+let rank_tot arbre n = match trouve n arbre with
+		 |true -> 
+		 let rec min arbre = match arbre with
+			|Vide -> failwith "no minimum"
+			|N(Vide,n,_) -> n
+			|N(g,_,_) -> min g
+		in let rec aux arbre n = match n, trouve n arbre with
+			|n, true when n = min arbre -> 1
+			|_, true -> 1 + (aux arbre (n-1))
+			|_, false -> aux arbre (n-1)
+		in aux arbre n 
+		|false -> failwith "l'élement n'est pas dans l'arbre";;
+
+rank_tot (N(N(N(Vide,1,Vide),3,N(Vide, 5, Vide)),6,N(Vide,7,N(Vide,9,Vide)))) 7;;
+
+let rec element_of_rank arbre m = match arbre with
+	|Vide -> ()
+	|N(g,n,d)-> if rank_tot n = m then n else (element_of_rank g n, element_of_rank d n)
 

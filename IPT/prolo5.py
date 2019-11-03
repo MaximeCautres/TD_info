e51fdabda63f8f4f4b113fa6f333ab53ad1172d8
Maxime Cautrès <maxime.cautres@gmail.com>
2019-11-03 15:10:13 +0100

    premiere moitie exo 5 prologin

--- IPT/prolo5.py
+++ IPT/prolo5.py
@@ -2,64 +2,51 @@ width = int(input())
 height = int(input())
 n = int(input())
 positions = []
-composition = [i for i in range(n)]
-maps = [[[] for _ in range(height + 2)] for _ in range(width + 2)]
+composition = []
+move = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+maps = [[-1 for _ in range(height + 2)] for _ in range(width + 2)]
+
+
+def parcours_contour(d, c, i, l):
+    if c == d:
+        return [k for k in range(n) if k in l]
+    else:
+        l.append(maps[c[0]][c[1]])
+        j = i - 1
+        while maps[c[0]+move[j % 4][0]][c[1]+move[j % 4][1]] == -1:
+            j += 1
+        return parcours_contour(d, [c[0]+move[j % 4][0], c[1]+move[j % 4][1]], j % 4, l)
+
+
+
 for i in range(n):
     positions.append([i + 1 for i in list(map(int, input().split()))])
     for x in range(positions[-1][0], positions[-1][2]):
-        for y in range(positions[-1][1] ,positions[-1][3]):
-            maps[x][y].append(i)
-alone = [True]*n
-for i in range(n):
-    if alone[i]:
-        checkneighbours(i,i)
-compo = []
-for i in range(n):
-	if composition[i] != -1:
-		compo.append(composition[i])
+        for y in range(positions[-1][1], positions[-1][3]):
+            maps[x][y] = i
 
-s += 1
-for c in compo:
-	if len(c) == 1:
-		s += min(abs(positions[c[0]][0]-positions[c[0]][2], abs(positions[c[0]][1]-positions[c[0]][3])))
-	else:
-		
+for i in maps:
+    print(i)
 
-def checkneighbours(d, k):
-    personnal_neighbours = []
-    for x in range(positions[k][0] - 1, positions[k][2] + 1):
-        if maps[x][positions[k][1] - 1] != -1:
-            if maps[x][positions[k][1] - 1] not in composition[d]:
-                composition[d].append(maps[x][positions[k][1] - 1])
-                personnal_neighbours.append(maps[x][positions[k][1] - 1])
-		if k!= d:
-			composition[k] = -1
-		alone[k] = False
-    for x in range(positions[k][0] - 1, positions[k][2] + 1):
-        if maps[x][positions[k][3] + 1] != -1:
-            if maps[x][positions[k][3] + 1] not in composition[d]:
-                composition[d].append(maps[x][positions[k][3] + 1])
-                personnal_neighbours.append(maps[x][positions[k][3] + 1])
-                if k != d:
-			composition[k] = -1
-		alone[k] = False
-    for y in range(positions[k][1] - 1, positions[k][3] + 1):
-        if maps[y][positions[k][0] - 1] != -1:
-            if maps[y][positions[k][0] - 1] not in composition[d]:
-                composition[d].append(maps[y][positions[k][0] - 1])
-                personnal_neighbours.append(maps[y][positions[k][0] - 1])
-                if k != d:
-			composition[k] = -1
-		alone[k] = False
-    for y in range(positions[k][1] - 1, positions[k][3] + 1):
-        if maps[y][positions[k][2] + 1] != -1:
-            if maps[y][positions[k][2] + 1] not in composition[d]:
-                composition[d].append(maps[y][positions[k][2] + 1])
-                personnal_neighbours.append(maps[y][positions[k][2] + 1])
-                if k != d:
-			composition[k] = -1
-		alone[k] = False
-    if personnal_neighbours:
-        for i in personnal_neighbours:
-            checkneighbours(d, i)
-    return ()
+for i in range(n):
+    s = True
+    for friends in composition:
+        if i in friends:
+            s = False
+    if s:
+        x = positions[i][0]
+        y = positions[i][1]
+        j = 0
+        s = True
+        while (maps[x + move[j % 4][0]][y + move[j % 4][1]] != -1 or
+               maps[x + move[(j+1) % 4][0]][y + move[(j+1) % 4][1]] == -1) and j < 4:
+            j += 1
+            s = s and (maps[x + move[j%4][0]][y  + move[j%4][1]] != -1)
+        if j == 4 and not s:
+            composition.append([i])
+        if j <= 3:
+            composition.append(parcours_contour([x, y], [x + move[(j+1) % 4][0],
+                                                         y + move[(j+1) % 4][1]], j + 1, [maps[x][y]]))
+
+print(composition)
+
